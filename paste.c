95b2300ddb269266e52cec49c6d4e55e75e50287
Matias <3959@holbertonschool.com>
2021-11-10 08:42:28 -0800

    fflush de buffer

--- paste.c
+++ paste.c
@@ -10,13 +10,20 @@
 
 int paste(char *buffer, char *s, int print_len)
 {
-	int i = 0;
+	int i = 0, print_len2;
 
+	if (print_len == 1024)
+	{
+		write(1, buffer, print_len);
+		print_len2 = print_len;
+		print_len = 0;
+
+	}
 	while (s && s[i]) /**mientras s exista pega el contenido en el buffer*/
 	{
 		buffer[i] = s[i];
 		print_len += 1;
 		i++;
 	}
-	return (print_len);
+	return (print_len + print_len2);
 }
